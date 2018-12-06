bf92dd012ccf8802303dfa05c7b19822c5dbe43b
Arjan de Graaf <arjang@SchoolMaster.nl>
2018-12-06 13:21:31 +0100

    dag 3 part 1

--- 2018/day03.py
+++ 2018/day03.py
@@ -1,5 +1,4 @@
 import os, csv, re
-from itertools import izip_longest
 
 script_dir = os.path.dirname(__file__)
 
@@ -12,25 +11,44 @@ with open(os.path.join(script_dir, input1)) as f:
 
 def get_claim_data(claim: str):
     matches = re.search('@ (\d+),(\d+): (\d+)x(\d+)', claim)
-    return [matches.group(1), matches.group(2), matches.group(3), matches.group(4)]
+    return [int(matches.group(1)), int(matches.group(2)), int(matches.group(3)), int(matches.group(4))]
 
-def create_entire_patch(claims: list):
-    xmax, ymax = 0, 0
-    for claim in claims:
-        claim_data = get_claim_data(claim)
-        xmax = max(xmax, claim_data[0] + claim_data[2])
-        ymax = max(ymax, claim_data[1] + claim_data[3])
-    entire_patch = [[0 for x in range(xmax)] for y in range(ymax)]
-
+# def create_entire_patch(claims: list):
+#     xmax, ymax = 0, 0
+#     for claim in claims:
+#         claim_data = get_claim_data(claim)
+#         xmax = max(xmax, claim_data[0] + claim_data[2])
+#         ymax = max(ymax, claim_data[1] + claim_data[3])
+#     entire_patch = [[0 for x in range(xmax)] for y in range(ymax)]
+#     return entire_patch
 
 def part1(claims: list) ->  int:
-    #entire_patch = create_entire_patch(claims)
+    patches, overlapping = {tuple()}, {tuple()}
     for claim in claims:
         claim_data = get_claim_data(claim)
+        for x in range(claim_data[0], claim_data[0] + claim_data[2]):
+            for y in range(claim_data[1], claim_data[1] + claim_data[3]):
+                coordinate = tuple([x,y])
+                if coordinate in patches:
+                    overlapping.add(coordinate)
+                patches.add(coordinate)
+    return len(overlapping)-1
 
-def part2(array):
-    return 'hoi'
+def part2(claims: list):
+    patches = {tuple()}
+    for claim in claims:
+        claim_data = get_claim_data(claim)
+        for x in range(claim_data[0], claim_data[0] + claim_data[2]):
+            for y in range(claim_data[1], claim_data[1] + claim_data[3]):
+                patches.add(tuple([x,y])
+
+    return len(overlapping)-1
 
 if __name__ == "__main__":
     
-    part1(data)
+    assert part1([
+        '#1 @ 1,3: 4x4', 
+        '#2 @ 3,1: 4x4', 
+        '#3 @ 5,5: 2x2']) == 4
+
+    print(part1(data))
